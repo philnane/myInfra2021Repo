49247d376a47c6c0e282396833a84c2a80c7d956
philnane <130239833+philnane@users.noreply.github.com>
2023-12-26 21:42:02 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "dataserviceschoolphil"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
